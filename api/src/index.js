3b8adce8d0008f027afdd79fc59c180190c0ef58
Rafael Alves <alvesrafael17@hotmail.com>
2020-09-27 22:10:48 -0300

    get Ingredients

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 
+import cors from 'cors';
+
 import IngredientRouter from './services/Ingredients/IngredientRouter';
 import RecipeRouter from './services/Recipes/RecipeRouter';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(IngredientRouter);
